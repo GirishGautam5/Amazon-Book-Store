14985a29e04b2b5a51a4a03f66d463a46df4332e
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-10-27 02:31:24 +0530

    added port

--- server/server.js
+++ server/server.js
@@ -21,4 +21,4 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
