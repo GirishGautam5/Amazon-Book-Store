c11844152fa8a4c47ecbe5389cb4579dae9be857
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-10-27 02:38:07 +0530

    added index.html path

--- server/server.js
+++ server/server.js
@@ -20,5 +20,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
