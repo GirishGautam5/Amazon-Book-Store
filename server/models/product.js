a08f229dfd64b0db4b119a05c3b94abe6400fb78
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-10-27 03:00:22 +0530

    added id in product schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   image: { type: String, required: true },
   author: { type: String, required: true },
-  
+  _id: { type: String, required: true},
   price: { type: Number, default: 0, required: true },
   category: { type: String, required: true },
   countInStock: { type: Number, default: 0, required: true },
