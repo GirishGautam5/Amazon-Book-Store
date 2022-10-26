3820487ceff59bc079c54ed882bc556f60edfd4d
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-10-27 01:57:04 +0530

    changes extension files

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Product from '../models/product';
+import Product from '../models/product.js';
 import { isAuth, isAdmin } from '../util';
 
 const router = express.Router();
