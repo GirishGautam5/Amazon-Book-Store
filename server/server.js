b1ad077f8d5ec87eb4c35528dab7c87923029c51
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-10-27 02:21:00 +0530

    removed commented code

--- server/server.js
+++ server/server.js
@@ -21,9 +21,4 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, '/client/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/client/build/index.html'))
-// );
 app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
